92913c2f94c8d2f934f0dfdfa79f88763e6562c5
Keyur146 <keyurbchauhan@gmail.com>
2023-08-12 15:08:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import List from "./components/List";
+import Alerts from "./components/Alerts";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem("list")))
+  }else{
+    return [];
+  }
+}
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIdEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list))
+  }, [list]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "danger", "Please Enter Value");
+    }else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return {...item, title: name}
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIdEditing(false);
+      setAlert(true, "success", "Value Changes");
+    }else{
+      showAlert(true, "success", "Item Added to the List");
+      const newItem = {id: new Date().getTime().toString(), title: name};
+      setList( list => [...list,  newItem ] );
+      setName("");
+    }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({show, type, msg});
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "Item Removed");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const editItem = list.find((item) => item.id === id);
+    setIdEditing(true);
+    setEditID(id);
+    setName(editItem.title);
+  };
+  
+  const clearList = () => {
+    showAlert(true, "danger", "Empty List");
+    setList([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <form onSubmit={handleSubmit}>
+        {alert.show && <Alerts {...alert} removeAlert={showAlert} list={list} />}
+        <h3 style={{ marginBottom: "1.5rem", textAlign: "center" }}>
+          Todo List
+        </h3>
+        <div className="mb-3 form">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="e.g. Drink Water"
+            onChange={(e) => setName(e.target.value)}
+            value={name} />
+          <button
+            type="submit"
+            className="btn btn-success">
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div style={{marginTop: "2rem"}}>
+          <List items={list} removeItem={removeItem} editItem={editItem}/>
+          <div className="text-center">
+            <button 
+            className="btn btn-warning"
+            onClick={clearList}
+            >Clear Items</button>
+          </div>
+        </div>
+      )}
+    </section>
   );
 }
 
---
+++ src/components/Alerts.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+const Alerts = ({type, msg, removeAlert, list}) => {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            removeAlert();
+        }, 3000);
+        return () => clearTimeout(timeout)
+    }, [list])
+    return <p className={`alert alert-${type}`}>{msg}</p>
+}
+
+
+export default Alerts
